2aea58b52cdf4a4ae8dccbf00fc55ff2a795d60c
Abhipurnea <abhipurnea3@gmail.com>
2023-05-17 11:39:31 +0530

    Login Form:add logout button and small change in UI

--- src/restaurant/management/forms/AdminForm.java
+++ src/restaurant/management/forms/AdminForm.java
@@ -20,14 +20,15 @@ public class AdminForm extends javax.swing.JFrame {
         itemsButton = new javax.swing.JButton();
         headerPanel = new javax.swing.JPanel();
         dashLabel = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(89, 92, 87));
         setUndecorated(true);
 
-        rootPanel.setBackground(new java.awt.Color(102, 102, 102));
+        rootPanel.setBackground(new java.awt.Color(51, 51, 51));
 
-        empButton.setBackground(new java.awt.Color(108, 108, 95));
+        empButton.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         empButton.setForeground(new java.awt.Color(255, 255, 255));
         empButton.setText("Manage Employee");
         empButton.addActionListener(new java.awt.event.ActionListener() {
@@ -36,7 +37,7 @@ public class AdminForm extends javax.swing.JFrame {
             }
         });
 
-        itemsButton.setBackground(new java.awt.Color(108, 108, 95));
+        itemsButton.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         itemsButton.setForeground(new java.awt.Color(255, 255, 255));
         itemsButton.setText("Manage Dish Items");
         itemsButton.addActionListener(new java.awt.event.ActionListener() {
@@ -47,7 +48,7 @@ public class AdminForm extends javax.swing.JFrame {
 
         headerPanel.setBackground(new java.awt.Color(13, 13, 18));
 
-        dashLabel.setBackground(new java.awt.Color(0, 108, 105));
+        dashLabel.setBackground(new java.awt.Color(0, 0, 0));
         dashLabel.setFont(new java.awt.Font("Century Gothic", 1, 20)); // NOI18N
         dashLabel.setForeground(new java.awt.Color(255, 255, 255));
         dashLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -57,24 +58,33 @@ public class AdminForm extends javax.swing.JFrame {
         headerPanel.setLayout(headerPanelLayout);
         headerPanelLayout.setHorizontalGroup(
             headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(dashLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(dashLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         headerPanelLayout.setVerticalGroup(
             headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(dashLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE)
         );
 
+        jButton1.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Logout");
+
         javax.swing.GroupLayout rootPanelLayout = new javax.swing.GroupLayout(rootPanel);
         rootPanel.setLayout(rootPanelLayout);
         rootPanelLayout.setHorizontalGroup(
             rootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(rootPanelLayout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(empButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(itemsButton)
-                .addContainerGap(19, Short.MAX_VALUE))
             .addComponent(headerPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(rootPanelLayout.createSequentialGroup()
+                .addGroup(rootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(rootPanelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(empButton)
+                        .addGap(25, 25, 25)
+                        .addComponent(itemsButton))
+                    .addGroup(rootPanelLayout.createSequentialGroup()
+                        .addGap(141, 141, 141)
+                        .addComponent(jButton1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         rootPanelLayout.setVerticalGroup(
             rootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -84,7 +94,9 @@ public class AdminForm extends javax.swing.JFrame {
                 .addGroup(rootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(empButton, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(itemsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -119,6 +131,7 @@ public class AdminForm extends javax.swing.JFrame {
     private javax.swing.JButton empButton;
     private javax.swing.JPanel headerPanel;
     private javax.swing.JButton itemsButton;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel rootPanel;
     // End of variables declaration//GEN-END:variables
 }
